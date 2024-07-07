fb37c9007ca1ddf0ab53a7578dfde8793302db55
SavanDalage <nekomimiwolf@gmail.com>
2024-07-07 21:17:36 +0200

    email-temlate

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const sgMail = require("@sendgrid/mail");
+const email = require("../email/email.js");
 
 const router = express.Router();
 sgMail.setApiKey(process.env.SENDGRID_PASSWORD);
@@ -19,49 +20,7 @@ router
         from: "nekomimiwolf@gmail.com",
         subject: "Formularz Treningowy - Zgłoszenie",
         text: "New form submission",
-        html: `<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>Formularz kontaktowy</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-        }
-        .container {
-            width: 50%;
-            margin: 0 auto;
-            border: 2px solid #000;
-            padding: 20px;
-            text-align: center;
-        }
-        .container p {
-            margin: 10px 0;
-        }
-        .container strong {
-            display: inline-block;
-            width: 150px;
-            text-align: right;
-            margin-right: 10px;
-        }
-    </style>
-</head>
-<body>
-    <div class="container">
-        <p><strong>Imię:</strong> ${data.imie}</p>
-        <p><strong>Nazwisko:</strong> ${data.nazwisko}</p>
-        <p><strong>E-mail:</strong> ${data.email}</p>
-        <p><strong>Telefon:</strong> ${data.phone}</p>
-        <p><strong>Czas:</strong> ${data.czas}</p>
-        <p><strong>Doświadczenie:</strong> ${data.doswiadczenie}</p>
-        <p><strong>Preferencje treningowe:</strong> ${
-          data.preferencje_treningu1 ? data.preferencje_treningu1 + " " : ""
-        } ${
-          data.preferencje_treningu2 ? data.preferencje_treningu2 + " " : ""
-        } ${data.preferencje_treningu3 ? data.preferencje_treningu3 : ""}</p>
-    </div>
-</body>
-</html>`,
+        html: email,
       };
 
       console.log(emailData);
